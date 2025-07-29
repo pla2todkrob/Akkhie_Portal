0f1ab93e6bea13fe1854f8321f966615dcaf93ac
pla2todkrob <pla2todkrob@gmail.com>
2025-07-29 13:27:01 +0700

    update

--- Portal.Shared/Models/ViewModel/EmployeeViewModel.cs
+++ Portal.Shared/Models/ViewModel/EmployeeViewModel.cs
@@ -1,12 +1,5 @@
 ﻿using Portal.Shared.Enums;
-using Portal.Shared.Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Portal.Shared.Models.ViewModel
 {
